496a319d3fdf19debac3bba0e5003a895213420b
kcopper8 <kcoooer8@gmail.com>
2022-08-14 14:55:19 +0900

    add init
    - cypress
    - react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import TodoList from "@containers/TodoList";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <TodoList />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TodoList />} />
+        <Route path="/add" element={<TodoList />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
