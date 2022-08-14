9d730c5786c981f8c57ec18b7dd4374392ae00c4
kcopper8 <kcoooer8@gmail.com>
2022-08-14 15:43:18 +0900

    feat: add uuid to todoList #5

--- src/store/todo.ts
+++ src/store/todo.ts
@@ -1,14 +1,15 @@
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
+import { v4 } from "uuid";
 import { Todo } from "./type";
 
 const $todoList = atom<Todo[]>({
   key: "TD-TODO-LIST",
   default: [
-    { title: "Racing car sprays burning fuel into crowd." },
-    { title: "Japanese princess to wed commoner." },
-    { title: "Australian walks 100km after outback crash." },
-    { title: "Man charged over missing wedding girl." },
-    { title: "Los Angeles battles huge wildfires." },
+    { id: "1", title: "Racing car sprays burning fuel into crowd." },
+    { id: "2", title: "Japanese princess to wed commoner." },
+    { id: "3", title: "Australian walks 100km after outback crash." },
+    { id: "4", title: "Man charged over missing wedding girl." },
+    { id: "5", title: "Los Angeles battles huge wildfires." },
   ],
 });
 
@@ -20,9 +21,15 @@ export const useTodoList = () => {
 export const useAddTodo = () => {
   const setTodoList = useSetRecoilState($todoList);
   return {
-    addTodo: (todo: Todo) => {
+    addTodo: (todo: Omit<Todo, "id">) => {
       setTodoList((prevTodoList) => {
-        return [...prevTodoList, todo];
+        return [
+          ...prevTodoList,
+          {
+            ...todo,
+            id: v4(),
+          },
+        ];
       });
     },
   };
